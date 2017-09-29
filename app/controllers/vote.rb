cdb2d198a74130158767aca10ac7c46f692642b1
Kara Kelley <kelley.15@gmail.com>
2017-09-29 17:35:13 -0500

    Working on down vote button

--- app/controllers/vote.rb
+++ app/controllers/vote.rb
@@ -1,7 +1,6 @@
 post '/questions/:id/votes' do
   @question = Question.find(params[:id])
-  p '*' * 90
-  p params
+
   if params[:vote_value] == "1"
     @question.up_vote(current_user.id)
 
@@ -15,6 +14,11 @@ post '/questions/:id/votes' do
 
   else
     @question.down_vote(current_user.id)
-    redirect "/questions/#{params[:id]}"
+
+    if request.xhr?
+      erb :'questions/_display_vote_count',  locals: {question: @question}, layout: false
+    else
+      redirect "/questions/#{params[:id]}"
+    end
   end
 end
