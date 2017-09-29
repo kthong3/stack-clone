72c45a0432224a7e5ddbae23e24d23ea711a639b
Kara Kelley <kelley.15@gmail.com>
2017-09-29 15:21:27 -0500

    Add up_vote and down_vote methods for Question class

--- app/models/question.rb
+++ app/models/question.rb
@@ -18,4 +18,34 @@ class Question < ActiveRecord::Base
     total_votes = self.votes.reduce(0) { |total, vote| total + vote.up_down_vote }
   end
 
+  def up_vote(user_id)
+    vote_search = self.votes.find_by poster_id: user_id
+    if vote_search
+      if vote_search.up_down_vote != 1
+        vote_search.up_down_vote = 1
+        vote_search.save
+        p "You already voted but we are changing your vote to up"
+      end
+        p "You already voted up! No change"
+    else
+      self.votes << Vote.create(up_down_vote: 1, poster_id: user_id)
+      p "We have created a new up vote for you"
+    end
+  end
+
+  def down_vote(user_id)
+    vote_search = self.votes.find_by poster_id: user_id
+    if vote_search
+      if vote_search.up_down_vote != -1
+        vote_search.up_down_vote = -1
+        vote_search.save
+        p "You already voted but we are changing your vote to down"
+      end
+        p "You already voted down! No change"
+    else
+      self.votes << Vote.create(up_down_vote: -1, poster_id: user_id)
+      p "We have created a new down vote for you"
+    end
+  end
+
 end
