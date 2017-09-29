d7852b463507f32412bfe07ace7cea161ad20551
Kara Kelley <kelley.15@gmail.com>
2017-09-29 18:08:16 -0500

    AJAXed it!

--- public/js/application.js
+++ public/js/application.js
@@ -140,4 +140,41 @@ $(document).ready(function() {
     })
   });
 
+    $('#answer-up-vote').on('submit', function(){
+    event.preventDefault();
+
+    var $form = $(this);
+    var url = $form.attr("action");
+    var method = $form.attr("method");
+    var data = {vote_value: 1};
+
+    var request = $.ajax({
+      url: url,
+      method: method,
+      data: data
+    })
+    request.done(function(response){
+      $('.total_votes').empty().append(response);
+    })
+  });
+
+  $('#answer-down-vote').on('submit', function(){
+    event.preventDefault();
+
+    var $form = $(this);
+    var url = $form.attr("action");
+    var method = $form.attr("method");
+    var data = {vote_value: -1};
+
+    var request = $.ajax({
+      url: url,
+      method: method,
+      data: data
+    })
+    request.done(function(response){
+      $('.total_votes').empty().append(response);
+    })
+  });
+
+
 });
